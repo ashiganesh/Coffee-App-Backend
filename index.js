932d491d658950ab92ca80d5afe48b760036d192
ashiganesh <ashiganesh352@gmail.com>
2025-06-19 13:27:15 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./Routes/authRoutes');
 const data = require('./database');
-const path = require('path');
+//const path = require('path');
 require('dotenv').config();
 const app = express();
 
@@ -11,7 +11,7 @@ app.use(express.json());
  
 data();
 app.use("/",router);
-app.use('/upload', express.static(path.join(__dirname,'uploads') ));
+app.use('/uploads', express.static('uploads'));
 
 app.listen(process.env.PORT || 3000,()=>{
 console.log(`server running in port number ${process.env.PORT}`)
