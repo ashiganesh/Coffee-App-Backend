cc9746ba58f176e4ef88c280a4cb6e3453cabdf5
ashiganesh <ashiganesh352@gmail.com>
2025-07-01 09:27:18 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+require('dotenv').config();
 const mongoose = require('mongoose');
 const router = require('./Routes/authRoutes');
 const data = require('./database');
 const cloudinary = require('cloudinary').v2;
 
 //const path = require('path');
-require('dotenv').config();
+
 const app = express();
 
 app.use(express.json());
@@ -13,9 +14,9 @@ app.use(express.json());
  
 data();
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: dsx5f8jzm,
+  api_key: 665234862273299,
+  api_secret: yf7wULxJFo1MU98mhdKGwJsJP4g,
 });
 app.use("/",router);
 //app.use('/uploads', express.static('uploads'));
